d66831ca4d438808461361f65ec271e8dbb9ef7c
kareemmmfayed <kareemf910zuku@gmail.com>
2025-12-25 18:03:53 +0200

    Set up app & database config

---
+++ src/main/java/com/excel/import_export_demo/ImportExportDemoApplication.java
@@ -0,0 +1,13 @@
+package com.excel.import_export_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportExportDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportExportDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excel/import_export_demo/ImportExportDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excel.import_export_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportExportDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
